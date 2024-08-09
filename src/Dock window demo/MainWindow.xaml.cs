19adbd864b1af619888d109cc9c4f85d1314ee94
FatRainbowPony <semfady@yandex.ru>
2024-08-09 14:32:33 +0700

    Initial commit

---
+++ src/Dock window demo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using DockWindow.Addons;
+using DockWindow.Windows;
+
+namespace DockWindowDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DockWindow.Windows.DockWindow
+    {
+        #region Constructors
+
+        #region Public
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Monitors.ItemsSource = MonitorInfo.GetAllMonitors();
+            Monitors.SelectedIndex = 0;
+
+            Modes.ItemsSource = new[]
+            {
+                DockMode.Left,
+                DockMode.Right,
+                DockMode.Top,
+                DockMode.Bottom
+            };
+            Modes.SelectedIndex = 2;
+        }
+        #endregion Public
+
+        #endregion Constructors
+
+        #region Methods
+
+        #region Private
+        private void CloseClick(object sender, RoutedEventArgs e) => Close();
+        #endregion Private
+
+        #endregion Methods
+    }
+}
---
+++ src/Dock window/Helpers/GlobalMouseHook.cs
@@ -0,0 +1,142 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using DockWindow.Addons;
+using static DockWindow.Helpers.NativeMethods;
+
+namespace DockWindow.Helpers
+{
+    public sealed class GlobalMouseHook
+    {
+        #region Fields
+
+        #region Private
+        private readonly object lockObject = new();
+        private readonly static object creationLock = new();
+        private static volatile GlobalMouseHook? instanceHook;
+        private MouseHookHandler? hookHandler;
+        private int handleHook;
+        private bool isStarted;
+        #endregion Private
+
+        #endregion Fields
+
+        #region Delegates
+
+        #region Public
+        public delegate void MouseActionHandler(GlobalMouseEventArgs e);
+        #endregion Public
+
+        #endregion Delegates
+
+        #region Events
+
+        #region Private
+        private event MouseActionHandler? MouseAction;
+        #endregion Private
+
+        #endregion Events
+
+        #region Constructors
+
+        #region Private
+        private GlobalMouseHook() { }
+        #endregion Private
+
+        #endregion Constructors
+
+        #region Destructors
+        ~GlobalMouseHook() => Stop();
+        #endregion Destructors
+
+        #region Methods
+
+        #region Public
+        public static GlobalMouseHook Create()
+        {
+            if (instanceHook == null)
+            {
+                lock (creationLock) 
+                {
+                    instanceHook = new GlobalMouseHook();
+                }
+            }
+
+            return instanceHook!;
+        }
+
+        public void Start()
+        {
+            if (!isStarted)
+            {
+                lock (lockObject) 
+                {
+                    if (MouseAction == null)
+                    {
+                        throw new Exception("Mouse action handler is not set");
+                    }
+
+                    if (handleHook == 0)
+                    {
+                        hookHandler = new MouseHookHandler((int nCode, int wParam, IntPtr lParam) => 
+                        {
+                            if (nCode >= 0 && MouseAction != null)
+                            {
+                                MouseAction(new GlobalMouseEventArgs(wParam, (MOUSEPOS)Marshal.PtrToStructure(lParam, typeof(MOUSEPOS))!));
+                            }
+
+                            return CallNextHookEx(handleHook, nCode, wParam, lParam);
+                        });
+
+                        handleHook = SetWindowsHookEx(WH_MOUSE_LL, hookHandler, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);
+                        if (handleHook == 0)
+                        {
+                            Stop();
+
+                            throw new Exception("Hook mouse failed");
+                        }
+                    }
+
+                    isStarted = true;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            if (isStarted)
+            {
+                lock (lockObject) 
+                {
+                    bool restMouse = true;
+
+                    if (handleHook != 0)
+                    {
+                        restMouse = UnhookWindowsHookEx(handleHook);
+                        handleHook = 0;
+                    }
+
+                    if (!restMouse)
+                    {
+                        throw new Exception("Unhook mouse failed");
+                    }
+
+                    MouseAction = null;
+                    isStarted = false;
+                }
+            }
+        }
+
+        public void AddMouseActionHandler(MouseActionHandler handler) => MouseAction += handler;
+
+        public void RemoveMouseAction(MouseActionHandler handler)
+        {
+            if (MouseAction != null)
+            {
+                MouseAction -= handler;
+            }
+        }
+        #endregion Public
+
+        #endregion Methods
+    }
+}
