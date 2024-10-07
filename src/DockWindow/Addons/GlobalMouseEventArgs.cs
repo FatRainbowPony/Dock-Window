1655221daf13ba67ece37f94f3bb51b59de2a9fe
FatRainbowPony <semfady@yandex.ru>
2024-10-07 11:53:21 +0700

    Added GlobalMouseEventArgs.cs

---
+++ src/DockWindow/Addons/GlobalMouseEventArgs.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using static DockWindow.Helpers.NativeMethods;
+
+namespace DockWindow.Addons
+{
+    public enum MouseState
+    {
+        None,
+        Move,
+        LeftButtonDown,
+        RightButtonDown,
+        MiddleButtonDown,
+        LeftButtonUp,
+        RightButtonUp,
+        MiddleButtonUp,
+        LeftButtonClick,
+        RightButtonClick,
+        MiddleButtonClick
+    }
+
+    public sealed class GlobalMouseEventArgs
+    {
+        #region Properties
+
+        #region Public
+        public MouseState MouseState { get; private set; }
+
+        public Point Location { get; private set; }
+        #endregion Public
+
+        #endregion Properties
+
+        #region Constructors
+
+        #region Internal
+        internal GlobalMouseEventArgs(int wParam, MOUSEPOS mPos)
+        {
+            MouseState = wParam switch
+            {
+                WM_MOUSEMOVE => MouseState.Move,
+                WM_LBUTTONDOWN => MouseState.LeftButtonDown,
+                WM_RBUTTONDOWN => MouseState.RightButtonDown,
+                WM_MBUTTONDOWN => MouseState.MiddleButtonDown,
+                WM_LBUTTONUP => MouseState.LeftButtonUp,
+                WM_RBUTTONUP => MouseState.RightButtonUp,
+                WM_MBUTTONUP => MouseState.MiddleButtonUp,
+                WM_LBUTTONDBLCLK => MouseState.LeftButtonClick,
+                WM_RBUTTONDBLCLK => MouseState.RightButtonClick,
+                WM_MBUTTONDBLCLK => MouseState.MiddleButtonClick,
+                _ => MouseState.None
+            };
+            Location = (Point)mPos.pt;
+        }
+        #endregion Internal
+
+        #endregion Constructors
+    }
+}
